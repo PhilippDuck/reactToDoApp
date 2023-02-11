5daac503ed9ceb07eec66532d0970385439e19ff
PhilippDuck <phil.mar.du@gmail.com>
2023-02-11 09:41:15 +0100

    changed navbar icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { GiPlasticDuck } from 'react-icons/gi'
 
 function NavBar() {
   return (
     <nav className='bg-neutral-800 p-4 w-full flex justify-center items-center gap-3 shadow-md'>
-        <GiPlasticDuck className='text-xl text-yellow-400'/>
+      <img width="20" src="/image/duckdo_512.png" alt="duckdo logo" />
+
         <h1 className='text-xl font-semibold'>DuckDo!</h1>
     </nav>
   )
